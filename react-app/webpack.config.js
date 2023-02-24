d2fd394a25a096cb9c15210a9943ff527dca92bb
urvashi29 <urvashigolu29@gmail.com>
2023-02-24 10:18:03 +0530

    changes in webpakc.config

--- react-app/webpack.config.js
+++ react-app/webpack.config.js
@@ -38,3 +38,33 @@ module.exports = {
     ],
   },
 };
+
+// rules: [
+//   {
+//     test: /\.(js|jsx)$/,
+//     exclude: /node_modules/,
+//     use: {
+//       loader: "babel-loader",
+//       options: {
+//         presets: ["@babel/preset-env"],
+//       },
+
+//     },
+//   },
+//   {
+//     test: /\.css$/i,
+//     use: ["style-loader", "css-loader"],
+//   },
+
+//   {
+//     test: /\.(jpe?g|gif|png|svg)$/i,
+//     use: [
+//       {
+//         loader: "url-loader","svg-url-loader",
+//         options: {
+//           limit: 10000,
+//         },
+//       },
+//     ],
+//   },
+// ];
